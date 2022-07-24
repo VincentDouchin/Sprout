7148f58ea620efd7634647e498d2a1fbcc2b3648
Vincent <vincent.douchin@gmail.com>
2022-07-24 11:37:02 +0200

    add fixed step engine

---
+++ src/Engine.js
@@ -0,0 +1,51 @@
+const Engine = function () {
+
+	let raf_handle
+	let accumulated_time = 0
+	let current_time = 0
+	let time_step = 1000 / 60
+
+
+	let state
+	let states
+	const cycle = (time_stamp) => {
+		raf_handle = window.requestAnimationFrame(cycle)
+
+		accumulated_time += time_stamp - current_time
+		current_time = time_stamp;
+
+		let updated = false
+
+		if (accumulated_time > 60) accumulated_time = time_step
+
+		while (accumulated_time >= time_step) {
+			state.update()
+			updated = true
+			accumulated_time -= time_step
+		}
+		if (updated) {
+			state.render()
+		}
+	}
+	const setState = (_state, options = {}) => {
+		if (!_state) return
+
+		const s = states[_state]
+		s.set(options)
+		state = s
+	}
+	return {
+		setState,
+		setStates(_states) {
+			states = _states
+		},
+		start() {
+			raf_handle = window.requestAnimationFrame(cycle)
+		},
+		stop() {
+			window.cancelAnimationFrame()
+		}
+	}
+}
+
+export default Engine

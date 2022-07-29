01d94a7bd66124225bc663439a1ff730547bfc82
Vincent <vincent.douchin@gmail.com>
2022-07-29 21:33:51 +0200

    added teleports to map

--- main.js
+++ main.js
@@ -8,82 +8,89 @@ import keys from './src/keys'
 import Engine from './src/Engine'
 import * as planck from 'planck';
 import { world, scene, render } from './src/Initialize'
-(async function () {
-	const engine = Engine()
 
-	//! Lights
-	const light = new THREE.AmbientLight(0xffffff)
-	scene.add(light)
-	light.position.set(0, 0, 200)
+const engine = Engine()
 
-
-	//! Objects
-	const map = getMap('map')
-	scene.add(map.meshTop)
-	scene.add(map.meshBottom)
-	map.meshBottom.renderOrder = 0
-	map.meshTop.renderOrder = 2
-	const character = await Character('Amélie')
-
-	scene.add(character.mesh)
-
-	//! Physics
-	const collisionsBody = world.createBody({
-		type: 'static',
-		position: { x: 0, y: 0 }
-	})
-	map.collisions.forEach(({ width, height, x, y }) => {
-		const newX = x - map.meshBottom.geometry.parameters.width / 2
-		const newY = map.meshBottom.geometry.parameters.height / 2 - y
-		collisionsBody.createFixture(planck.Box(width / 2, height / 2, planck.Vec2(newX, newY), 0.0), 0.0);
-	})
-
-	const clock = new THREE.Clock()
-	const controller = Controller(keys)
-
-	const run = {
-		update() {
-			world.step(clock.getDelta() * 1000)
+//! Lights
+const light = new THREE.AmbientLight(0xffffff)
+scene.add(light)
+light.position.set(0, 0, 200)
 
 
-			camera.position.x = character.mesh.position.x
-			camera.position.y = character.mesh.position.y
-			camera.lookAt(character.mesh.position)
+//! Objects
+const map = getMap('map')
+scene.add(map.meshTop)
+scene.add(map.meshBottom)
+map.meshBottom.renderOrder = 0
+map.meshTop.renderOrder = 2
+const character = await Character('Amélie')
+window.planck = planck
+scene.add(character.mesh)
+
+//! Physics
+const collisionsBody = world.createBody({
+	type: 'static',
+	position: { x: 0, y: 0 }
+})
+map.collisions.forEach(({ width, height, x, y }, i) => {
+	const newX = x - map.meshBottom.geometry.parameters.width / 2
+	const newY = map.meshBottom.geometry.parameters.height / 2 - y
+	collisionsBody.createFixture(planck.Box(width / 2, height / 2, planck.Vec2(newX, newY), 0.0), 0.0);
+})
+
+const teleportBody = world.createBody({
+	type: 'static',
+	position: { x: 0, y: 0 }
+})
+map.teleports.forEach(({ width, height, x, y }) => {
+	const newX = x - map.meshBottom.geometry.parameters.width / 2
+	const newY = map.meshBottom.geometry.parameters.height / 2 - y
+	teleportBody.createFixture(planck.Box(width / 2, height / 2, { x: newX, newY }, 0), 0)
+})
+const clock = new THREE.Clock()
+const controller = Controller(keys)
+
+const run = {
+	update() {
+		world.step(clock.getDelta() * 1000)
 
 
-
-
-			if (controller.left.active) {
-				character.move('left')
-			}
-			if (controller.right.active) {
-				character.move('right')
-			}
-			if (controller.up.active) {
-				character.move('up')
-			}
-			if (controller.down.active) {
-				character.move('down')
-			}
-
-			//! Colisions
-			character.update()
+		camera.position.x = character.mesh.position.x
+		camera.position.y = character.mesh.position.y
+		camera.lookAt(character.mesh.position)
 
 
 
-		},
-		render() {
-			render()
 
-		},
-		set() {
-
+		if (controller.left.active) {
+			character.move('left')
 		}
+		if (controller.right.active) {
+			character.move('right')
+		}
+		if (controller.up.active) {
+			character.move('up')
+		}
+		if (controller.down.active) {
+			character.move('down')
+		}
+
+		//! Colisions
+		character.update()
+
+
+
+	},
+	render() {
+		render()
+
+	},
+	set() {
+
 	}
-	engine.setStates({ run })
-	engine.setState('run')
-	engine.start()
+}
+engine.setStates({ run })
+engine.setState('run')
+engine.start()
 
 
-
-}())
--- src/AssetManager.js
+++ src/AssetManager.js
@@ -15,18 +15,21 @@ const AssetManager = await (async () => {
     const sourceLevels = import.meta.globEager('../assets/levels/*.json')
     const sourceTilesets = import.meta.globEager('../assets/tilesets/*.json')
     const sourceImages = import.meta.globEager('../assets/images/**/*.png')
+    const sourceTemplates = import.meta.globEager('../assets/object templates/**/*.json')
 
     const loadTileSet = async (tileset) => ({ ...tileset, img: await loadImage(tileset.image.replace('../', '../assets/')) })
     const assignTilesets = (tilesets) => (map) => ({ ...map, tilesets: map.tilesets.map(tileset => ({ ...tileset, ...tilesets[getFileName(tileset.source)] })) })
     const tilesets = await mapToFileName(sourceTilesets, loadTileSet)
     const levels = await mapToFileName(sourceLevels, assignTilesets(tilesets))
     const images = await mapToFileName(sourceImages)
+    const templates = await mapToFileName(sourceTemplates)
 
 
     return {
         levels,
         tilesets,
         images,
+        templates,
         async load(name) {
             this.images[name] = await loadImage(images[name].default)
             return this.images[name]

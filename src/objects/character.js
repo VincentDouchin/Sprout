b2f5ff0d94a5d4ca104ed76d8aab64619a1fbbe5
Vincent Douchin <vincent.douchin@gmail.com>
2022-07-29 17:54:51 +0200

    Add files via upload

--- src/objects/character.js
+++ src/objects/character.js
@@ -1,7 +1,7 @@
 import getPlane from "./plane";
 import AssetManager from "../AssetManager";
 import Buffer from "../utils/buffer";
-import * as CANNON from 'cannon-es'
+import * as planck from 'planck'
 
 import { Box3, BoxHelper, Vector3, Mesh, PlaneGeometry, MeshBasicMaterial } from "three";
 const friction = 0.9
@@ -62,7 +62,6 @@ const Character = async (_name) => {
 
 			}; break
 		}
-
 	}
 	const collisionBox = new Mesh(new PlaneGeometry(16, 16), new MeshBasicMaterial())
 	// const boxhelper = new BoxHelper(collisionBox, 0xffff00);
